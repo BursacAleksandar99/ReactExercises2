82edbb6d4879d3c40cb584935b029488705acd29
Aleksandar Bursac <stevica.bursac.no.1@gmail.com>
2024-05-09 15:19:38 +0200

    VEZBA6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Text } from './Text'; 
 
 function App() {
+  
+  const[showText, setshowText] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+          <button onClick={() => {
+            setshowText(!showText);
+          }}
+          >
+            Show Text
+
+          </button>
+          {showText && <Text/>}
     </div>
-  );
+  )
+
 }
 
 export default App;
